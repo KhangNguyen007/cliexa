c9f7c9f30b52fd96f5bef32d1e575fc1893423d7
Nick Kai <nickkai@Nicks-MacBook-Pro-3.local>
2020-11-11 23:52:35 -0700

    Progress bar and cliexa logo

--- public/javascripts/Main.js
+++ public/javascripts/Main.js
@@ -43,20 +43,25 @@ function drawAll(){
     }
 
     for(let i = 0 ; i < rects.length;i++){
-        rectsSVG[i].update(rects[i].x, rects[i].y, rects[i].height, rects[i].width, "#FFFFFF", '1')
+        rectsSVG[i].update(rects[i].x, rects[i].y, rects[i].width,rects[i].height, "#FFFFFF", '1')
         titleTextSVG[i].update(rects[i].x, rects[i].y, '#444444', rects[i].title)
-        yesRectSVG[i].updateWithOnClick(rects[i].x, rects[i].y + heightSize/3, heightSize/3, widthSize/3, 'green', '1', "populateRec(1)")
-        noRectSVG[i].updateWithOnClick(rects[i].x + rects[i].width - widthSize/3, rects[i].y + heightSize/3, heightSize/3, widthSize/3, 'red', '1', "populateRec(0)")
+        yesRectSVG[i].updateWithOnClick(rects[i].x, rects[i].y + heightSize/3, widthSize/3,heightSize/3, 'green', '1', "populateRec(1)")
+        noRectSVG[i].updateWithOnClick(rects[i].x + rects[i].width - widthSize/3, rects[i].y + heightSize/3, widthSize/3,heightSize/3, 'red', '1', "populateRec(0)")
+        yesTextSVG[i].update(rects[i].x, rects[i].y + rects[i].height/2, '#000', "Yes")
+        noTextSVG[i].update(rects[i].x + rects[i].width - 25, rects[i].y+ rects[i].height/2,'#000', "No")
     }
 }
 function draw(index) {
 
 
 
-    rectsSVG[index].update(rects[index].x, rects[index].y, rects[index].height, rects[index].width, "#FFFFFF", '1')
+    rectsSVG[index].update(rects[index].x, rects[index].y, rects[index].width,rects[index].height, "#FFFFFF", '1')
     titleTextSVG[index].update(rects[index].x, rects[index].y + 50, '#444444', rects[index].title)
-    yesRectSVG[index].updateWithOnClick(rects[index].x, rects[index].y + heightSize / 3, heightSize / 3, widthSize / 3, 'green', '1', "populateRec(1)")
-    noRectSVG[index].updateWithOnClick(rects[index].x + rects[index].width - widthSize / 3, rects[index].y + heightSize / 3, heightSize / 3, widthSize / 3, 'red', '1', "populateRec(0)")
+    yesRectSVG[index].updateWithOnClick(rects[index].x, rects[index].y + heightSize / 3, widthSize / 3,heightSize / 3, 'green', '1', "populateRec(1)")
+    noRectSVG[index].updateWithOnClick(rects[index].x + rects[index].width - widthSize / 3, rects[index].y + heightSize / 3, widthSize / 3,heightSize / 3, 'red', '1', "populateRec(0)")
+    yesTextSVG[index].update(rects[index].x, rects[index].y + rects[index].height/2, '#000', "Yes")
+    noTextSVG[index].update(rects[index].x + rects[index].width - 25, rects[index].y+ rects[index].height/2,'#000', "No")
+
 
     for(let i = 1 ; i < rects.length;i++){
         linesSVG[i-1].update(rects[i-1].x+rects[i-1].width/2,rects[i-1].y+rects[i-1].height/2,rects[i].x+rects[i].width/2,rects[i].y+rects[i].height/2,"red")
@@ -70,9 +75,13 @@ function clear(){
     titleTextSVG[titleTextSVG.length-1].remove()
     yesRectSVG[yesRectSVG.length-1].remove()
     noRectSVG[noRectSVG.length-1].remove()
+    yesTextSVG[yesTextSVG.length-1].remove()
+    noTextSVG[noTextSVG.length-1].remove()
     if(linesSVG.length >= 1)
         linesSVG[linesSVG.length-1].remove()
     rectsSVG.pop()
+    yesTextSVG.pop()
+    noTextSVG.pop()
     titleTextSVG.pop()
     yesRectSVG.pop()
     noRectSVG.pop()
@@ -90,7 +99,6 @@ function selectShape(){
 }
 
 function populateRec(answer){
-    console.log("Expand new rectangle: ",answer)
     let index
     let title
     //Here is the logic to expand question
@@ -115,12 +123,14 @@ function populateRec(answer){
     if(rects[rects.length-1].x + $('#mainPanel').width() >= $('#svg').width()){
         let width = $('#svg').width()
         $('#svg').width(width+width)
-        console.log("Upgrading new svg:")
-        console.log($('#svg').width())
     }
+    //Update progress bar
+    test_progress_bar += 25
+    $('#progress-bar').width(test_progress_bar + '%')
+    $('#progress-bar').text(test_progress_bar +'%')
     //Should change to focus and not focus
     drawTheShape(title)
-
+    prev = index
     slide(0)
 }
 function slide(slide){
@@ -145,9 +155,8 @@ function reAlign(){
     rects[0].update(x,y,shape_width,shape_height)
     //Update new position for all when realign
     for(let i = 1; i < rects.length; i++){
-        rects[i].update(rects[i-1].x + width,rects[i-1].y,shape_height,shape_height)
+        rects[i].update(rects[i-1].x + width,rects[i-1].y,shape_width,shape_height)
     }
-    console.log("new rects:",rects)
     x_scale=1
     y_scale=1
     translateX=0
@@ -155,8 +164,10 @@ function reAlign(){
     panZoom.reset()
     panZoom.zoom(1)
     drawAll()
+    //1 means go back to
     slide(1)
 }
+
 function drawTheShape(title){
     let width =  Math.floor(mainPanelWidth - $("#leftPanel").width() - $("#rightPanel").width());
     let height = Math.floor(mainPanelHeight - $("#topPanel").height());
@@ -165,23 +176,37 @@ function drawTheShape(title){
     let y = 0.05 *height
     let shape_height = height -0.1*height
     //Init the first shape
-    let newRect = new Rectangle(x,y,shape_width,shape_height,"#FFFFFF",false, title,rects.length)
+    let newRect
+    if(rects.length === 0) {
+        newRect = new Rectangle(x, y, shape_width, shape_height, "#FFFFFF", false, title, rects.length)
+    }
+    else{
+        newRect = new Rectangle(rects[rects.length-1].x + width, rects[rects.length-1].y, shape_width, shape_height, "#FFFFFF", false, title, rects.length)
+        let newLineSVG = new LineSVG()
+        linesSVG.push(newLineSVG)
+    }
     let newRectSVG = new RectangleSVG()
     let newTitleTextSVG = new TextSVG()
     let newYesRectSVG   = new RectangleSVG()
-    let yesTextSVG = new TextSVG()
+    let newYesTextSVG = new TextSVG()
     let newNoRectSVG    = new RectangleSVG()
-    let noTextSVG = new TextSVG()
-    newRectSVG.update(newRect.x,newRect.y,width,height,newRect.fill,0)
+    let newNoTextSVG = new TextSVG()
+
+
+    newRectSVG.update(newRect.x,newRect.y,newRect.width,newRect.height,newRect.fill,0)
     newTitleTextSVG.update(newRect.x, newRect.y+ 50, '#000', newRect.title)
     newYesRectSVG.updateWithOnClick(newRect.x, newRect.y + height/2, 25, 25, 'green', '1', "populateRec(1)")
-    yesTextSVG.update(newRect.x, newRect.y + height/2, '#000', "Yes")
+    newYesTextSVG.update(newRect.x, newRect.y + height/2, '#000', "Yes")
     newNoRectSVG.updateWithOnClick(newRect.x + newRect.width - 25, newRect.y, 25, 25, 'red', '1', "populateRec(0)")
-    noTextSVG.update(newRect.x + newRect.width - 25, newRect.y,'#000', "No")
+    newNoTextSVG.update(newRect.x + newRect.width - 25, newRect.y+ height/2,'#000', "No")
+
     rects.push(newRect)
     rectsSVG.push(newRectSVG)
     titleTextSVG.push(newTitleTextSVG)
     yesRectSVG.push(newYesRectSVG)
     noRectSVG.push(newNoRectSVG)
+    yesTextSVG.push(newYesTextSVG)
+    noTextSVG.push(newNoTextSVG)
     draw(newRect.id)
+
 }
